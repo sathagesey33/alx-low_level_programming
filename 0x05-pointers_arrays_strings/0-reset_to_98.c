5532bba7147ffe63154f4d968db66227556087a8
sathagesey33 <abdukadirmuhanad@gmail.com>
2023-03-27 03:33:07 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Write a function that takes a pointer to an int
+ * Return: Always 0.
+*/
+
+int main(void)
+{
+Prototype: void reset_to_98(int *n);
+
+Return(0)
+}
