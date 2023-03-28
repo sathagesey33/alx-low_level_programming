c28c1db3dbea94e22f640c16079bcf90aa225d61
sathagesey33 <abdukadirmuhanad@gmail.com>
2023-03-28 15:16:05 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,14 +6,15 @@
   * Return: void
   */
 
-void rev_string(char *s);
-
+void rev_string(char *s)
 {
 	int len = 0, index = 0;
 	char temp;
 
 	while (s[index++])
+	{
 	len++;
+	}
 
 	for (index = len - 1; index >= len / 2; index--)
 	{
