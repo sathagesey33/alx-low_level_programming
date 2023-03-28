f82c03abd2545d5ffe2b6b5a80aae18c0fdbb685
sathagesey33 <abdukadirmuhanad@gmail.com>
2023-03-28 08:20:12 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+  *_strcpy - copy a string
+  * @dest: Destination value
+  * @src: Source value
+  * Return: the pointer to detest
+  */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+
+	{
+
+	dest[i] = src[i];
+	}
+
+	dest[i++] = '\0';
+
+	return (dest);
+	}
