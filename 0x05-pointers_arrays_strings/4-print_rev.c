3ebe7bc9e262dd48606cc18809336a390bf78636
sathagesey33 <abdukadirmuhanad@gmail.com>
2023-03-27 17:12:52 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,7 +10,6 @@
 void print_rev(char *s)
 
 {
-
 	int c = 0;
 
 	while (s[c] != '\0')
@@ -19,11 +18,11 @@ void print_rev(char *s)
 	c++;
 	}
 
-	for (c -= !; c >= 0; c--)
+	for (c -= 1; c >= 0; c--)
 
 	{
 
-	_puchar(s[c]);
+	_putchar(s[c]);
 
 	}
 
